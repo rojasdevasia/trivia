180aead967d4960fa94bd71bf8dda993ebae147c
rojasdevasia <rojas.devasia@gmail.com>
2023-03-04 21:09:12 +0530

    review comments incorporated

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,9 +2,9 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
-
 from flaskr import create_app
 from models import setup_db, Question, Category
+from settings import TEST_DB_NAME, TEST_DB_USER, TEST_DB_PASSWORD
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -14,8 +14,10 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://postgres/postgres".format('localhost:5432', self.database_name)
+        # self.database_name = "trivia_test"
+        # self.database_path = "postgres://postgres/postgres".format('localhost:5432', self.database_name)
+        self.database_name= TEST_DB_NAME
+        self.database_path = 'postgresql://{}:{}@{}/{}'.format(TEST_DB_USER,TEST_DB_PASSWORD,'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
